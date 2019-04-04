4a7c07e5bcae6b63200c9cb8698155ae5da9c8dd
Jake Loew <jakeloew1@gmail.com>
2019-04-04 15:02:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   } else {
-    everybody = []
     for(var i = 0; i < katzDeliLine.length; i++){
     everybody.push(" " + (i + 1) + ". " + katzDeliLine[i]);
     } return "The line is currently:" + everybody.join();
