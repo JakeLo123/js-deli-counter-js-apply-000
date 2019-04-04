652d1890303a94d787581c50301ddc0b443f9989
Jake Loew <jakeloew1@gmail.com>
2019-04-04 14:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function nowServing(katzDeliLine){
   }
 }
 
-var everybody = [];
+
 function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "The line is currently empty."
   } else {
+    var everybody = [];
     for(var i = 0; i < katzDeliLine.length; i++){
     everybody.push(" " + (i + 1) + ". " + katzDeliLine[i]);
     } return "The line is currently:" + everybody.join();
